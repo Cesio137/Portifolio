e50a7cb273a9081d8a482e42efbc2e5b08500501
Nathan Miguel <nathan.cm77@gmail.com>
2025-06-24 19:16:58 -0300

    fixed unused values issue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,6 @@
 <script setup lang="ts">
-import { useDark, useToggle } from "@vueuse/core";
-import HelloWorld from "./components/HelloWorld.vue";
 import Navbar from "./components/Navbar.vue";
 
-const isDark = useDark({
-    onChanged(isDark) {
-        if (isDark) {
-            document.documentElement.classList.add("dark");
-        } else {
-            document.documentElement.classList.remove("dark");
-        }
-    },
-});
-const toggleDark = useToggle(isDark);
 </script>
 
 <template>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { useDark, useToggle } from "@vueuse/core";
+import { useDark } from "@vueuse/core";
 import { SunIcon, MoonIcon } from "@heroicons/vue/20/solid";
 
 defineProps<{ msg: string }>();
