230fd82cde2f7f415061b34b15ecdc4c6f367b7a
Nathan Miguel <nathan.cm77@gmail.com>
2025-06-25 20:34:51 -0300

    added card as initial project

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 import Navbar from "./components/Navbar.vue";
+import Card from "./components/Card.vue";
 
 </script>
 
 <template>
-    <Navbar msg="navbar"></Navbar>
-    <h1 class="text-3xl font-bold text-center">Hello world!</h1>
+    <Navbar></Navbar>
+    <main class="flex min-h-screen flex-col items-center justify-center">
+        <Card></Card>
+    </main>
 </template>
 
 <style scoped>
