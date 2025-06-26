692931548c516f16c210a7ca4a02c0647bfc179c
Nathan Miguel <nathan.cm77@gmail.com>
2025-06-26 18:12:50 -0300

    responsive card

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
+import './css/card.css'
+import './css/navbar.css'
+import './css/responsive.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
