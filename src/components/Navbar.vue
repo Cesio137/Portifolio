0bd3bb984617dea4c2d6dd0613b40a29a435f386
Nathan Miguel <nathan.cm77@gmail.com>
2025-06-27 23:55:17 -0300

    added hamburguer menu

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { useDark } from "@vueuse/core";
-import { SunIcon, MoonIcon } from "@heroicons/vue/20/solid";
+import { SunIcon, MoonIcon, Bars3Icon, XMarkIcon } from "@heroicons/vue/20/solid";
 
 const isDark = useDark({
     onChanged(isDark) {
@@ -15,21 +15,37 @@ const isDark = useDark({
 </script>
 
 <template>
-    <nav>
-        <div class="nav-links">
+    <nav class="flex flex-col">
+        <div class="w-full flex">
+            <div class="nav-links">
+                <input type="checkbox" id="navmenu" class="navmenu-input"/>
+                <label for="navmenu">
+                    <div>
+                        <Bars3Icon class="hamburger-icon" />
+                        <XMarkIcon class="x-icon" />
+                    </div>
+                </label>
+                <div>
+                    <a>INICIO</a>
+                    <a>PROJETOS</a>
+                </div>
+            </div>
+            <div class="nav-configs">
+                <input type="checkbox" id="darkmode-toggle" class="darkmode-input" v-model="isDark" />
+                <label for="darkmode-toggle" class="darkmode-label">
+                    <div class="darkmode-toggle-circle">
+                        <MoonIcon class="darkmode-toggle-moon" />
+                        <SunIcon class="darkmode-toggle-sun" />
+                    </div>
+                </label>
+            </div>
+        </div>
+        <div class="nav-mobile-links">
             <a>INICIO</a>
             <a>PROJETOS</a>
-        </div>
-        <div class="nav-configs">
-            <input type="checkbox" id="darkmode-toggle" class="darkmode-input" v-model="isDark" />
-            <label for="darkmode-toggle" class="darkmode-label">
-                <div class="darkmode-toggle-circle">
-                    <MoonIcon class="darkmode-toggle-moon" />
-                    <SunIcon class="darkmode-toggle-sun" />
-                </div>
-            </label>
         </div>
     </nav>
 </template>
 
-<style scoped></style>
+<style scoped>
+</style>
