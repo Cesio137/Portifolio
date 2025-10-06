59da9598c7e1b32a3976ff05a3875d68ebef3927
Nathan Miguel <nathan.cm77@gmail.com>
2025-10-06 11:33:43 -0300

    corrigido a sensação de travamento e removido link para X

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -157,21 +157,10 @@ const { t } = useI18n();
                     </svg>
                     <div />
                 </a>
-                <a target="_blank" href="https://x.com/NathanCmig">
-                    <svg data-v-5fb5722d="" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                        viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                        stroke-linejoin="round" class="lucide lucide-twitter-icon lucide-twitter w-5 h-5"
-                        aria-hidden="true">
-                        <path
-                            d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z">
-                        </path>
-                    </svg>
-                    <div />
-                </a>
                 <a target="_blank" href="https://www.linkedin.com/in/nathan-miguel-488b462b1/">
                     <svg data-v-5fb5722d="" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                         viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                        stroke-linejoin="round" class="lucide lucide-linkedin-icon lucide-linkedin w-5 h-5"
+                        stroke-linejoin="round" class="lucide lucide-linkedin-icon lucide-linkedin w-6 h-5"
                         aria-hidden="true">
                         <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
                         <rect width="4" height="12" x="2" y="9"></rect>
