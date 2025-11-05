f2a4c59d28ba981e2232a9f4590cf6b0223a0fa0
Cesio137 <nathan.cm77@gmail.com>
2025-11-05 17:35:52 -0300

    add X social link

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -157,6 +157,14 @@ const { t } = useI18n();
                     </svg>
                     <div />
                 </a>
+                <a target="_blank" href="https://x.com/nathancmig">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" width="24px" height="24px"
+                        class="bi bi-twitter-x" viewBox="-4 -4 24 24">
+                        <path
+                            d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z" />
+                    </svg>
+                    <div />
+                </a>
                 <a target="_blank" href="https://www.linkedin.com/in/nathan-miguel-488b462b1/">
                     <svg data-v-5fb5722d="" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                         viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
